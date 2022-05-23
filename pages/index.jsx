45934159fed7ddbd6cfad151179e0c428da1452f
daygil <dk80251@gmail.com>
2022-05-23 16:15:42 +0900

    START

---
+++ pages/index.jsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title> ValueUp v.2 </title>{" "}
+        <meta
+          name="description"
+          content="대외활동, 동아리 등 대학생, 취준생들을 위한 플랫폼"
+        />
+      </Head>
+      <main className={styles.main}>
+        <h1> ValueUp v .2 </h1>
+      </main>
+    </div>
+  );
+}
